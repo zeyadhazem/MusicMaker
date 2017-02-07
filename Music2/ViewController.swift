1f7e83fcfcd6f2d9191f3568b2a9e9abb63610fb
Zeyad Saleh <zeyadhazemsaleh@gmail.com>
2017-02-07 16:58:33 -0500

    added Kicks and Snares

--- Music2/ViewController.swift
+++ Music2/ViewController.swift
@@ -10,33 +10,65 @@ import UIKit
 import AudioKit
 
 class ViewController: UIViewController {
-
-    @IBAction func toggleSound(_ sender: UIButton) {
-        if oscillator.isPlaying {
-            oscillator.stop()
-            sender.setTitle("Play Sine Wave", for: .normal)
-        } else {
-            oscillator.start()
-            sender.setTitle("Stop Sine Wave", for: .normal)
-        }
+    
+    var kick = AKSynthKick()
+    var snare = AKSynthSnare(duration: 0.07)
+    
+    @IBAction func kickBtn(_ sender: UIButton) {
+        kick.play(noteNumber: 60, velocity: 100)
+        kick.stop(noteNumber: 60)
+        print("kick")
+    }
+    @IBAction func snareBtn(_ sender: UIButton) {
+        snare.play(noteNumber: 60, velocity: 50)
+        snare.stop(noteNumber: 60)
+        print("snare")
     }
    
     @IBAction func frequencySlider(_ sender: UISlider) {
-        oscillator.frequency = Double (sender.value * 880.0)
+        oscillator.frequency = Double (sender.value * 261.63)
     }
     
     var oscillator = AKOscillator()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        oscillator.amplitude = 0.1
-        AudioKit.output = oscillator
+        kick = AKSynthKick()
+        snare = AKSynthSnare(duration: 0.07)
+        let mix = AKMixer(kick, snare)
+        let reverb = AKReverb(mix)
+        AudioKit.output = reverb
         AudioKit.start()
+        reverb.loadFactoryPreset(.mediumRoom)
+        
+        kick.play(noteNumber: 20, velocity: 100)
+        kick.stop(noteNumber: 20)
+        print("kick")
+        
+        snare.play(noteNumber: 21, velocity: 50)
+        snare.stop(noteNumber: 21)
+        print("snare")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    
+//    func playCChord(){
+//        bank.play(noteNumber: 72, velocity: 80)
+//        bank.play(noteNumber: 76, velocity: 80)
+//        bank.play(noteNumber: 79, velocity: 80)
+//        sleep(2)
+//        bank.stop(noteNumber: 72)
+//        bank.stop(noteNumber: 76)
+//        bank.stop(noteNumber: 79)
+//    }
+    
+    func createAndStartOscillator(frequency: Double) -> AKOscillator {
+        let oscillator = AKOscillator()
+        oscillator.frequency = frequency
+        oscillator.start()
+        return oscillator
     }
 
 
