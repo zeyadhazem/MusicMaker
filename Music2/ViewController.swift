c25dbe1fdd52f55cf60407558681c2962191948c
Zeyad Saleh <zeyadhazemsaleh@gmail.com>
2017-03-04 03:33:45 -0500

    Notes and Chords (Major and Minor Scales) can now be played

--- Music2/ViewController.swift
+++ Music2/ViewController.swift
@@ -11,13 +11,70 @@ import AudioKit
 
 class ViewController: UIViewController {
     
-    var kick = AKSynthKick()
-    var snare = AKSynthSnare(duration: 0.07)
-    var flute = AKFlute()
-    var mandolin = AKMandolin()
-    let scale = [0, 2, 4, 5, 7, 9, 11, 12]
-    var pluckPosition = 0.2
     
+    // Global Vars
+    var kick        = AKSynthKick()
+    var snare       = AKSynthSnare(duration: 0.07)
+    var flute       = AKFlute()
+    var mandolin    = AKMandolin()
+    var oscillator  = AKOscillator()
+    
+    let scale           = [0, 2, 4, 5, 7, 9, 11, 12]
+    var pluckPosition   = 0.2
+    
+    enum Notes: Int {
+        case C = 0, C_Sharp, D, D_Sharp, E, F, F_Sharp, G, G_Sharp, A, A_Sharp, B
+    }
+    
+    enum Scales {
+        case Major, Minor
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        kick = AKSynthKick()
+        
+        snare = AKSynthSnare(duration: 0.07)
+        
+        flute = AKFlute()
+        
+        mandolin = AKMandolin()
+        mandolin.detune = 1
+        mandolin.bodySize = 1
+        mandolin.presetElectricGuitarMandolin()
+        
+        let mix = AKMixer(kick, snare, flute, mandolin)
+        let reverb = AKReverb(mix)
+        
+        AudioKit.output = reverb
+        AudioKit.start()
+        
+        reverb.loadFactoryPreset(.largeRoom)
+    }
+    
+    func playNote(note: Notes, generator: Int, octave: Int){
+        mandolin.fret(noteNumber: note.rawValue + 12*octave, course: generator)
+        mandolin.pluck(course: generator, position: pluckPosition, velocity: 127)
+    }
+    
+    func playNote(note: Int, generator: Int, octave: Int){
+        mandolin.fret(noteNumber: note + 12*octave, course: generator)
+        mandolin.pluck(course: generator, position: pluckPosition, velocity: 127)
+    }
+    
+    func playChord(note: Notes, scale: Scales){
+        switch scale {
+        case .Major:    //1 - 4 - 7
+            playNote(note: note.rawValue, generator: 1, octave: 4)
+            playNote(note: (Int)((note.rawValue + 4).truncatingRemainder(dividingBy: 12)), generator: 2, octave: 4)
+            playNote(note: (Int)((note.rawValue + 7).truncatingRemainder(dividingBy:12)), generator: 3, octave: 4)
+        case .Minor:    //1 - 3 -7
+            playNote(note: note.rawValue, generator: 1, octave: 4)
+            playNote(note: (Int)((note.rawValue + 3).truncatingRemainder(dividingBy: 12)), generator: 2, octave: 4)
+            playNote(note: (Int)((note.rawValue + 7).truncatingRemainder(dividingBy:12)), generator: 3, octave: 4)        }
+    }
+
     @IBAction func kickBtn(_ sender: UIButton) {
         kick.play(noteNumber: 60, velocity: 100)
         kick.stop(noteNumber: 60)
@@ -32,99 +89,99 @@ class ViewController: UIViewController {
    
     @IBAction func ANote(_ sender: UIButton) {
         if (isNote.isOn){
-            playANote()
+            playNote(note: .A, generator: 1, octave: 4)
         }
         else{
-            playAChord()
+            playChord(note: .A, scale: .Major)
         }
     }
     @IBAction func ASharpNote(_ sender: UIButton) {
         if (isNote.isOn){
-            playASharpNote()
+            playNote(note: .A_Sharp, generator: 1, octave: 4)
         }
         else{
-            playASharpChord()
+            playChord(note: .A_Sharp, scale: .Major)
         }
     }
     @IBAction func BNote(_ sender: UIButton) {
         if (isNote.isOn){
-            playBNote()
+            playNote(note: .B, generator: 1, octave: 4)
         }
         else{
-            playBChord()
+            playChord(note: .B, scale: .Major)
         }
     }
     @IBAction func CNote(_ sender: UIButton) {
         if (isNote.isOn){
-            playCNote()
+            playNote(note: .C, generator: 1, octave: 4)
         }
         else{
-            playCChord()
+            playChord(note: .C, scale: .Major)
         }
     }
     @IBAction func CSharpNote(_ sender: UIButton) {
         if (isNote.isOn){
-            playCSharpNote()
+            playNote(note: .C_Sharp, generator: 1, octave: 4)
         }
         else{
-            playCSharpChord()
+            playChord(note: .C_Sharp, scale: .Major)
         }
     }
     @IBAction func DNote(_ sender: UIButton) {
         if (isNote.isOn){
-            playDNote()
+            playNote(note: .D, generator: 1, octave: 4)
         }
         else{
-            playDChord()
+            playChord(note: .D, scale: .Major)
         }
     }
     @IBAction func DSharpNote(_ sender: UIButton) {
         if (isNote.isOn){
-            playDSharpNote()
+            playNote(note: .D_Sharp, generator: 1, octave: 4)
         }
         else{
-            playDSharpChord()
+            playChord(note: .D_Sharp, scale: .Major)
         }
     }
     @IBAction func ENote(_ sender: UIButton) {
         if (isNote.isOn){
-            playENote()
+            playNote(note: .E, generator: 1, octave: 4)
         }
         else{
-            playEChord()
+            playChord(note: .E, scale: .Major)
         }
     }
     
     @IBAction func FNote(_ sender: UIButton) {
-        if (isNote.isOn){
-            playFNote()
+        if (isNote.isOn) {
+            playNote(note: .F, generator: 1, octave: 4)
         }
-        else{
-            playFChord()
+        else {
+            playChord(note: .F, scale: .Major)
         }
     }
     @IBAction func FSharpNote(_ sender: UIButton) {
         if (isNote.isOn){
-            playFSharpNote()
+            playNote(note: .F_Sharp, generator: 1, octave: 4)
         }
         else{
-            playFSharpChord()
+            playChord(note: .F_Sharp, scale: .Major)
         }
     }
     @IBAction func GNote(_ sender: UIButton) {
         if (isNote.isOn){
-            playGNote()
+            playNote(note: .G, generator: 1, octave: 4)
         }
         else{
-            playGChord()
+            playChord(note: .G, scale: .Major)
         }
     }
     @IBAction func GSharpNote(_ sender: UIButton) {
         if (isNote.isOn){
-            playGSharpNote()
+            playNote(note: .G_Sharp, generator: 1, octave: 4)
         }
         else{
-            playGSharpChord()
+            playChord(note: .G_Sharp, scale: .Major)
         }
     }
     
@@ -137,24 +194,6 @@ class ViewController: UIViewController {
    
     @IBAction func frequencySlider(_ sender: UISlider) {
         oscillator.frequency = Double (sender.value * 261.63)
-    }
-    
-    var oscillator = AKOscillator()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        kick = AKSynthKick()
-        snare = AKSynthSnare(duration: 0.07)
-        flute = AKFlute()
-        mandolin = AKMandolin()
-        mandolin.detune = 1
-        mandolin.bodySize = 1
-        mandolin.presetElectricGuitarMandolin()
-        let mix = AKMixer(kick, snare, flute, mandolin)
-        let reverb = AKReverb(mix)
-        AudioKit.output = reverb
-        AudioKit.start()
-        reverb.loadFactoryPreset(.mediumRoom)
     }
     
     func playRandomFluteNote(){
@@ -193,121 +232,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func playANote(){
-        mandolin.fret(noteNumber: 57, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    func playASharpNote(){
-        mandolin.fret(noteNumber: 58, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    func playBNote(){
-        mandolin.fret(noteNumber: 59, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    func playCNote(){
-        mandolin.fret(noteNumber: 48, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    func playCSharpNote(){
-        mandolin.fret(noteNumber: 49, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    func playDNote(){
-        mandolin.fret(noteNumber: 50, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    func playDSharpNote(){
-        mandolin.fret(noteNumber: 51, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    func playENote(){
-        mandolin.fret(noteNumber: 52, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    func playFNote(){
-        mandolin.fret(noteNumber: 53, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    func playFSharpNote(){
-        mandolin.fret(noteNumber: 54, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    func playGNote(){
-        mandolin.fret(noteNumber: 55, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    func playGSharpNote(){
-        mandolin.fret(noteNumber: 56, course: 1)
-        mandolin.pluck(course: 1, position: pluckPosition, velocity: 127)
-    }
-    
-    
-    
-    func playAChord(){
-        playANote()
-        playCSharpNote()
-        playENote()
-    }
-    func playASharpChord(){
-        playASharpNote()
-        playDNote()
-        playFNote()
-    }
-    func playBChord(){
-        playBNote()
-        playDSharpNote()
-        playFSharpNote()
-    }
-    func playCChord(){
-        playCNote()
-        playENote()
-        playGNote()
-    }
-    func playCSharpChord(){
-        playCSharpNote()
-        playFChord()
-        playGSharpNote()
-    }
-    func playDChord(){
-        playDNote()
-        playFSharpNote()
-        playANote()
-    }
-    func playDSharpChord(){
-        playDSharpNote()
-        playENote()
-        playASharpNote()
-    }
-    func playEChord(){
-        playENote()
-        playGSharpNote()
-        playBNote()
-    }
-    func playFChord(){
-        playFNote()
-        playANote()
-        playCNote()
-    }
-    func playFSharpChord(){
-        playFSharpNote()
-        playASharpNote()
-        playCSharpNote()
-    }
-    func playGChord(){
-        playGNote()
-        playBNote()
-        playDNote()
-    }
-    func playGSharpChord(){
-        playGSharpNote()
-        playCNote()
-        playDSharpNote()
-    }
-    
-    
-    
-    
+
     
     
     
